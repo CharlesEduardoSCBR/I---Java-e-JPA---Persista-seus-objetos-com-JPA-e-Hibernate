1f8594330eb8fe7a46d9e0592a41ee0dd6868282
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-03-03 20:07:25 -0300

    C1M4.11 - Preparando para o proximo episodio

--- src/br/com/caelum/financas/util/JPAUtil.java
+++ src/br/com/caelum/financas/util/JPAUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JPAUtil {
-	private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("contas-mysql");
+	private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("financas");
 	
 	public EntityManager getEntityManager(){
 		return entityManagerFactory.createEntityManager();
