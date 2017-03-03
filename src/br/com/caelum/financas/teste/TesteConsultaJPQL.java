396734489cd972284f2bf6fd7d714d7ab376ae32
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-03-03 20:54:55 -0300

    C1M5.10 - Trabalhando com Positional Parameter Notation

---
+++ src/br/com/caelum/financas/teste/TesteConsultaJPQL.java
@@ -0,0 +1,42 @@
+package br.com.caelum.financas.teste;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteConsultaJPQL {
+
+	public static void main(String[] args) {
+		EntityManager manager;
+		Conta conta;
+		Query query;
+		List<Movimentacao> movimentacoes;
+		
+		manager = new JPAUtil().getEntityManager();
+		
+		conta = new Conta();
+		conta.setId(1);
+		
+		query = manager.createQuery("SELECT m "
+									+ " FROM Movimentacao m "
+									+ "WHERE m.conta.id = ?1"
+									+ "  AND m.tipoMovimentacao = ?2");
+		
+		query.setParameter(1, conta.getId());
+		query.setParameter(2, TipoMovimentacao.ENTRADA);
+		
+		movimentacoes = query.getResultList();
+		
+		for (Movimentacao m : movimentacoes) {
+			System.out.println("\nDescricao ..: " + m.getDescricao());
+			System.out.println("Valor .....: R$ " + m.getValor());
+		}
+		manager.close();
+	}
+}
