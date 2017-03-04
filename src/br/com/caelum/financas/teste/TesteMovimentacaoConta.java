c61f52e61923fd49ad666e2d9c0e34ccccebc1e0
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-03-04 12:41:17 -0300

    C1M6.02.04 - ...

--- src/br/com/caelum/financas/teste/TesteMovimentacaoConta.java
+++ src/br/com/caelum/financas/teste/TesteMovimentacaoConta.java
@@ -17,11 +17,12 @@ public class TesteMovimentacaoConta {
 		Query query;
 
 		manager = new JPAUtil().getEntityManager();
-		query = manager.createQuery("SELECT c FROM Conta C");
+		query = manager.createQuery("SELECT c FROM Conta c JOIN FETCH c.movimentacoes");
 		contas = query.getResultList();
 		
 		for (Conta conta : contas) {
-			System.out.println("Numero de movimentaçoes ...: " + conta.getMovimentacoes().size());
+			System.out.println(conta.getId() + " - " + conta.getTitular());
+			System.out.println("Numero de movimentaçoes ...: " + conta.getMovimentacoes().size() + "\n");
 		}
 	}
 }
